42b05bb874b6dc436bf01430b660cc46faf13d20
aniramcg <57899557+aniramcg@users.noreply.github.com>
2020-04-28 11:01:18 +0200

    s

--- utils/utils.py
+++ utils/utils.py
@@ -176,9 +176,9 @@ def random_crop(image, label, crop_height, crop_width):
     if (crop_width <= image.shape[1]) and (crop_height <= image.shape[0]):
         pdf_im = np.ones(label.shape) # y is a mask
         pdf_im[label>0]=10000 # pdf aquí es un peso. Por ejemplo 10000. 
-        cropw = int(crop_width/2)
-        croph = int(crop_height/2)
-        pdf_im = pdf_im[cropw:(image.shape[0]-cropw),croph:(image.shape[1]-croph)] # limit the coordinates in which a centroid can lay
+#        cropw = int(crop_width/2)
+#        croph = int(crop_height/2)
+        pdf_im = pdf_im[0:crop_width,0:crop_height)] # limit the coordinates in which a centroid can lay
         prob = np.float32(pdf_im)
         prob = prob.ravel()/np.sum(prob) # convert the 2D matrix into a vector and normalize it so you create a distribution of all the possible values between 1 and prod(pdf.shape)(sum=1)
         choices = np.prod(pdf_im.shape) 
@@ -189,8 +189,8 @@ def random_crop(image, label, crop_height, crop_width):
             y = coordinates[1][0] # pdf second coordinate corresponds to the "y" axis = height
         else:
             print('I am here')
-            x = random.randint(cropw, int(label.shape[1])-cropw)
-            y = random.randint(croph, int(label.shape[0])-croph)
+            x = random.randint(0, crop_widht)
+            y = random.randint(0, crop_height)
             print(y)
         if len(label.shape) == 3:
             return image[y:y+crop_height, x:x+crop_width, :], label[y:y+crop_height, x:x+crop_width, :]
