7b4fa40ad4e1046467458d6214c456bfe8ae4e50
esgomezm <>
2020-06-02 12:31:41 +0200

    correct data augmentation

---
+++ plot_results.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+
+path2results = '/home/esgomezm/Documents/3D-PROTUCEL/Semantic-Segmentation-Suite/SERVER-RESULTS/'
+folder = 'sampling_reduce_v00'
+# 'reduce_lr_v01'
+# 'constante_lr_v01'
+
+data = pd.read
