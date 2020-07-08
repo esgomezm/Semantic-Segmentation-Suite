8a4fc9fbe56c2194f83b19ed6d2fc1e375ffb4d5
aniramcg <57899557+aniramcg@users.noreply.github.com>
2020-07-08 16:28:19 +0200

    Update train_fc_densenet_v00.sh

--- train_fc_densenet_v00.sh
+++ train_fc_densenet_v00.sh
@@ -1,2 +1,2 @@
 pip install scipy==1.1.0
-python train2.py --dataset "data" --num_epochs '800' --learning_rate '0.01' --batch_size '1' --continue_training True --checkpoint 'fc_dense_net' --reduce_lr 'True'
+python train4drive.py --dataset "data" --model 'FC-DenseNet103' --num_epochs '1000' --learning_rate '0.005' --batch_size '1' --continue_training True --checkpoint 'fc_dense_net' --reduce_lr 'True'
